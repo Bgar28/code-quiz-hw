03ddb67173b2c63d34e9d01e9c91dbd0f4a52acc
Brian Garland <briangarlandk@gmail.com>
2021-10-28 22:47:53 -0500

    Added first question after click  with the choices as buttons

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 // variables
 const startButton = document.querySelector('#start-btn')
 const timerEl = document.querySelector('#timer') 
+const starterEl = document.querySelector('#starter-content')
+const quizContent = document.querySelector('#quiz-content')
+const questionResult = document.querySelector('#question-result')
+let timer = 60
+let currentQuestion = 0
 timerEl.textContent = 0
 
+// when start button is clicked, the timer starts its countdown from 60 secs
 startButton.addEventListener('click', function(){
-    let timer = 60
+   // add a class name of "hide" to get rid of the starter content after the button is clicked 
+    starterEl.classList.add('hide');
+
+    const container = document.createElement('div')
+    quizContent.append(container)
+    const question = document.createElement('p')
+    container.append(question)
+    question.textContent = quiz[currentQuestion].question
+    const choicesGroup = document.createElement('ul')
+    container.append(choicesGroup)
+    for (let i=0; i < quiz[currentQuestion].choices.length; i++){
+        const choiceEL = document.createElement('li')
+        const buttonEL = document.createElement('button')
+        buttonEL.textContent = quiz[currentQuestion].choices[i]
+        buttonEL.addEventListener('click', function(event){
+            const textContent = event.target.firstChild.textContent;
+            if(textContent===quiz[currentQuestion].answer){
+                questionResult.textContent = 'Correct'
+                // add a show class to this 
+            }
+            else{
+                questionResult.textContent = 'Incorrect'
+                  // add a show class to this 
+            }
+
+            // always increment to next question
+            currentQuestion++
+            // go to next question
+            question.textContent = quiz[currentQuestion].question
+
+            // settimeout to remove class from questionResults
+            
+            
+        })
+        choiceEL.append(buttonEL)
+        choicesGroup.append(choiceEL)
+    
+    }
+
+
+
     const timerInterval = setInterval(function(){
     timer--;
     timerEl.textContent = timer
@@ -13,12 +59,13 @@ startButton.addEventListener('click', function(){
     }
 
     },1000)
+
 })
 
 // variable containing quiz questions/answers
 const quiz = [
     {
-        question: 'Inside which element for we link our JavaScript file?',
+        question: 'Inside which element do we link our JavaScript file?',
         choices: ['<link>', '<script>', '<title>', '<a>'],
         answer: '<script>'
     },
