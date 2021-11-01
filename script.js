038bed20878f21a7b773cb35e698cfd9f7e5c2d3
Brian Garland <briangarlandk@gmail.com>
2021-11-01 18:22:26 -0500

    Added input field after all quiz questions are clicked

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const questionResult = document.querySelector('#question-result')
 let timer = 60
 let currentQuestion = 0
 timerEl.textContent = 0
+const submitBtn = document.createElement('button')
+var intials = document.createElement('input')
+
 
 // when start button is clicked, the timer starts its countdown from 60 secs
 startButton.addEventListener('click', function(){
@@ -19,34 +22,68 @@ startButton.addEventListener('click', function(){
     container.append(question)
     question.textContent = quiz[currentQuestion].question
     const choicesGroup = document.createElement('ul')
+    choicesGroup.classList.add('choicesList')
     container.append(choicesGroup)
     for (let i=0; i < quiz[currentQuestion].choices.length; i++){
         const choiceEL = document.createElement('li')
         const buttonEL = document.createElement('button')
         buttonEL.textContent = quiz[currentQuestion].choices[i]
         buttonEL.addEventListener('click', function(event){
-            const textContent = event.target.firstChild.textContent;
-            if(textContent===quiz[currentQuestion].answer){
-                questionResult.textContent = 'Correct'
-                // add a show class to this 
-            }
-            else{
-                questionResult.textContent = 'Incorrect'
-                  // add a show class to this 
-            }
+            if(currentQuestion < quiz.length-1){
 
-            // always increment to next question
-            currentQuestion++
-            // go to next question
-            question.textContent = quiz[currentQuestion].question
-
-            // settimeout to remove class from questionResults
-            
+                const textContent = event.target.firstChild.textContent;
+                if(textContent===quiz[currentQuestion].answer){
+                    questionResult.textContent = 'Correct'
+                }
+                else{
+                    questionResult.textContent = 'Incorrect'
+                }
+                    
+                    // always increment to next question
+                    currentQuestion++
+                    // go to next question
+                    question.textContent = quiz[currentQuestion].question
+                    changeChoices()
+                    // buttonEL.textContent = quiz[currentQuestion].choices[i]
+                    // settimeout to remove class from questionResults
+                    const msgTimer = setTimeout(function(){
+                        questionResult.textContent = ''
+                    },1000)
+            } else {
+                // after last question is clicked, user is able to enter their highscore
+                container.remove()
+                question.remove()
+                const end = document.createElement('h1')
+                end.textContent = 'You have completed the quiz! Please enter your intials to save your highscore.'
+                quizContent.append(end)
+                const form = document.createElement('form')
+                quizContent.append(form)
+                intials.setAttribute('type', 'text')
+                intials.setAttribute('name', 'name')
+                intials.setAttribute('placeholder', 'Name or intials')
+                form.append(intials)
+                var userInput = intials.value
+                submitBtn.setAttribute('id', 'submit')
+                submitBtn.textContent = 'Submit'
+                form.append(submitBtn)
+                submitBtn.addEventListener('click', function(e, userInput){
+                    e.preventDefault()
+                    console.log(userInput)
+                })
+            }
+                
             
         })
         choiceEL.append(buttonEL)
         choicesGroup.append(choiceEL)
     
+    }
+
+    function changeChoices(){
+        for (let j=0; j < choicesGroup.children.length; j++){
+            choicesGroup.children[j].children[0].textContent = quiz[currentQuestion].choices[j]
+        }
+
     }
 
 
